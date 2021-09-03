dec18278493fc06947619302b85ae56169593ee4
styx11 <1654630248@qq.com>
2021-09-03 10:28:54 +0800

    feat: update linter constant tip

--- client/src/lib/constant.ts
+++ client/src/lib/constant.ts
@@ -9,3 +9,15 @@ export const ActivationLanguage = [
 	"onLanguage:html",
 	"onLanguage:vue"
 ]
+
+// 自定义国际化内容选项
+export const CUSTOM_PICK_OPTION = '自定义'
+
+// 自定义国际化输入框内容
+export const CUSTOM_INPUT_CONTENT = "intl.formatMessage({ id: '' })"
+
+// 选择国际化文本 placeholder
+export const CUSTOM_PICK_PLACEHOLDER = '请选择用来替换的国际化文本'
+
+// 自定义国际化输入框 placeholder
+export const CUSTOM_INPUT_PLACEHOLDER = '请输入自定义的国际化文本内容'
