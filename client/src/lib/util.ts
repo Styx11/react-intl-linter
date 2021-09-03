cbb91f8f72177b804d773e312147b0c298501d38
styx11 <1654630248@qq.com>
2021-09-03 10:29:31 +0800

    feat: update get translate result with progress demo

--- client/src/lib/util.ts
+++ client/src/lib/util.ts
@@ -1,6 +1,7 @@
+import { window as Window, ProgressLocation } from 'vscode'
 import { DocumentSelector } from 'vscode-languageclient'
 
-import { ActivationLanguage } from './constant'
+import { ActivationLanguage, CUSTOM_PICK_OPTION } from './constant'
 
 // 获取 package.json 文件配置的激活事件 activationEvents 对应的语言
 export const getDocumentSelector = (): DocumentSelector =>
@@ -14,3 +15,20 @@ export const getDocumentSelector = (): DocumentSelector =>
 		return language ? prevLangs.concat({ scheme: 'file', language: language }) : prevLangs
 	}, [])
 };
+
+// 获取文本翻译结果 - 通过 进度弹框 的形式
+export const getTranslateResultsWithProgress = async (searchText: string): Promise<string[]> =>
+{
+	return await Window.withProgress({
+		cancellable: false,
+		location: ProgressLocation.Notification,
+		title: `正在获取“${searchText}”的翻译结果...`,
+	}, async () =>
+	{
+		// 获取供选择的 Options
+		// 这是最终的返回结果
+		return await new Promise<string[]>((resolve) =>
+			setTimeout(() => resolve(['Visual Studio', 'Visual Studio Code', CUSTOM_PICK_OPTION]), 3000)
+		)
+	})
+}
