a183a59030d203d5ee0d6053cc27ad9d6ba2e5de
styx11 <1654630248@qq.com>
2021-09-07 10:42:50 +0800

    feat: update client constant

--- client/src/lib/constant.ts
+++ client/src/lib/constant.ts
@@ -10,14 +10,20 @@ export const ActivationLanguage = [
 	"onLanguage:vue"
 ]
 
+export const getIntlMessage = (intlId: string) => `intl.formatMessage({ id: '${intlId}' })`
+
+export const CUSTOM_INTL_ID_REGX = /[A-Z_]+/
+
+// json 文件的缩进数
+export const JSON_SPACE = 4
+
 // 自定义国际化内容选项
 export const CUSTOM_PICK_OPTION = '自定义'
 
-// 自定义国际化输入框内容
-export const CUSTOM_INPUT_CONTENT = "intl.formatMessage({ id: '' })"
-
 // 选择国际化文本 placeholder
-export const CUSTOM_PICK_PLACEHOLDER = '请选择用来替换的国际化文本'
+export const CUSTOM_PICK_PLACEHOLDER = '请选择用来替换的国际化文本 id 内容'
 
 // 自定义国际化输入框 placeholder
-export const CUSTOM_INPUT_PLACEHOLDER = '请输入自定义的国际化文本内容'
+export const CUSTOM_INPUT_PLACEHOLDER = '请输入自定义的国际化 id 内容'
+
+export const INVALID_CUSTOM_ID_MESSAGE = '国际化 id 只能由大写字符或下划线组成'
