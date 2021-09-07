c9d66adcfca708123e1ad9f7a75ceb7028336de8
styx11 <1654630248@qq.com>
2021-09-07 11:03:17 +0800

    chore: update client configs

---
+++ client/src/lib/intl/index.ts
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-anonymous-default-export */
+import zh_CN from "./zh_CN.json";
+import en_US from "./en_US.json";
+
+export type ILocales = 'en-US' | 'zh-CN'
+
+export function getLocales(lang: ILocales)
+{
+	switch (lang)
+	{
+		case ('en-US'):
+			return en_US
+		case ('zh-CN'):
+			return zh_CN
+		default:
+			return en_US
+	}
+}
+
+export default {
+	"en-US": en_US,
+	"zh-CN": zh_CN
+}
