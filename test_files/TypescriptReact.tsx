d2c3eedffa3e08c7aaf48d686f74264f20b71efa
styx11 <1654630248@qq.com>
2022-02-08 09:57:31 +0800

    test: update linter test files

--- test_files/TypescriptReact.tsx
+++ test_files/TypescriptReact.tsx
@@ -1,25 +1,29 @@
+// ri-lint-disable
 "UPPERCASE TEXT"
 
 '一个文本'
 
+// ri-lint-enable
 "这是另一个文本"
 
 '这是一个特殊的文件'
 
+// ri-lint-disable
 'react-intl=你好，{status: 无敌的}{name: Fred 哥}';
 
-'$=你好，{nameTitle: Fred 哥}'
+'$=你好，{nameTitle: Fred 哥}' // ri-lint-enable-line
 
 '$=共 {total: 100} 条记录，第 {page: 1}/{totalPage: 10} 页'
 
 '$=添加'
 
+// ri-lint-enable
 '添加'
 
 // https://github.com/Styx11/react-intl-linter/issues/7
 const validMessage = true ? '' : '你好'; const followMessage = true ? '你好' : ''
 
-const message = `${"添加"}hellllllo${"添加"}`
+const message = `${"添加"}hellllllo${"添加"}` /* ri-lint-disable-line */
 
 const testMsg = `${'添加'}hellllllo${'一个文本'}hellllllo{"这是另一个文本"}`
 
