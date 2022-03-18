908efb5ff8e47b7c4e961dc85253d87fde2564f2
styx11 <1654630248@qq.com>
2022-03-18 10:35:37 +0800

    feat: update get baidu query source func

--- client/src/lib/translate/baidu.ts
+++ client/src/lib/translate/baidu.ts
@@ -1,8 +1,7 @@
 import { URLSearchParams } from 'url';
 import { v4 as uuidv4 } from 'uuid';
 import md5 from 'blueimp-md5';
-import axios from 'axios';
-import { capitalize } from './util';
+import { SupportLanguage } from './TranslateManager';
 
 export interface BaiduResponse
 {
@@ -20,14 +19,14 @@ export interface BaiduResponse
  * @param {boolean} [isChinese] - 查询字符串是否为中文
  * @return {*} string - 请求链接
  */
-export const getBaiduSource = (query: string, isChinese?: boolean): string =>
+export const getBaiduSource = (query: string, from: SupportLanguage, to: SupportLanguage): string =>
 {
 	const q = query.toLocaleLowerCase();
 	const appid = '20191014000341469';
 	const key = '4GEOkedoeuLlSiwypAoD';
 	const baidu = 'http://api.fanyi.baidu.com/api/trans/vip/translate';
-	const from = 'auto';
-	const to = isChinese ? 'en' : 'zh'; // to can't be auto
+	// const from = 'auto';
+	// const to = isChinese ? 'en' : 'zh'; // to can't be auto
 	const salt = uuidv4(); // UUID
 	const str = appid + q + salt + key;
 	const dict = '1'
@@ -51,30 +50,3 @@ export const getBaiduSource = (query: string, isChinese?: boolean): string =>
 
 	return source;
 };
-
-
-/**
- * 翻译中文文本，错误将会被抛出
- *
- * @param {string} query - 将要翻译的中文文本
- * @return {*}  {Promise<string[]>} - 翻译结果数组
- */
-export const getBaiduZhTranslation = async (query: string): Promise<string[]> =>
-{
-	if (!query) return []
-
-	const source = getBaiduSource(query, true)
-
-	const result = await axios.get<BaiduResponse>(source)
-
-	const {
-		trans_result,
-		error_code,
-	} = result.data
-
-	if (result.data.error_code) throw new Error('翻译错误码' + error_code)
-
-	console.log('translationResult', result)
-
-	return trans_result.map(r => capitalize(r.dst))
-}
